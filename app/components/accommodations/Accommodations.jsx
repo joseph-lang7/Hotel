a58b30e93aa351006fa3a586c3ee34fcd56f3e83
joseph <langj6757@gmail.com>
2024-05-30 21:15:22 -0700

    Adjust height of Accommodations container

--- app/components/accommodations/Accommodations.jsx
+++ app/components/accommodations/Accommodations.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import RectangleButton from "../rectangle-button/RectangleButton";
-import Image from "next/image";
 const Accommodations = () => {
   return (
-    <div className="max-w-screen flex justify-center md:h-[1000px] xl:h-[700px] pt-20 px-5">
+    <div className="max-w-screen flex justify-center h-auto py-20 px-5">
       <div className="flex max-w-[450px] sm:max-w-[500px] md:max-w-[700px] lg:max-w-[900px] xl:max-w-[1200px]  2xl:max-w-[1400px] flex-col lg:flex-row gap-5 w-full">
         {/* Text container */}
         <div className="w-full ">
@@ -25,20 +24,16 @@ const Accommodations = () => {
         {/* Image container */}
         <div className=" w-full max-w-[800px] h-[400px] md:h-[500px] relative">
           <div className="absolute bottom-0 w-full md:w-[500px] lg:w-full xl:w-[500px] h-[300px] md:h-[350px]">
-            <Image
+            <img
               src="https://images.pexels.com/photos/6782567/pexels-photo-6782567.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Bedroom"
-              layout="fill"
-              objectFit="cover"
             />
           </div>
           <div className="absolute top-0 right-0 h-[250px] w-[270px] md:w-[350px] md:h-[300px]">
             {" "}
-            <Image
+            <img
               src="https://images.pexels.com/photos/21345931/pexels-photo-21345931/free-photo-of-vulcano.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Bedroom"
-              layout="fill"
-              objectFit="cover"
             />
           </div>
         </div>
