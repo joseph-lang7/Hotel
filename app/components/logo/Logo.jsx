e4add043d78c848b1feef3c0829f0fb93e8286b7
joseph <langj6757@gmail.com>
2024-06-01 10:45:09 -0700

    Refactor MobileNav

--- app/components/logo/Logo.jsx
+++ app/components/logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 const Logo = ({ textColor }) => {
   return (
-    <div className="flex items-center font-bold z-50">
+    <div className="flex items-center font-bold z-30">
       <Image src="/logo.png" alt="Hotel logo" width={60} height={60} />
       <h4 className={`text-xl ${textColor}`}>HOTEL</h4>
     </div>
--- app/components/navbar/MobileNav.jsx
+++ app/components/navbar/MobileNav.jsx
@@ -3,10 +3,8 @@ const MobileNav = ({ setOpen, isOpen, navItems }) => {
   return (
     <ul
       className={`${
-        isOpen
-          ? "translate-x-0 max-w-screen h-screen bg-white text-black transition-transform duration-700 flex-col flex justify-center items-center gap-5 text-2xl lg:hidden fixed inset-0 z-40"
-          : "translate-x-[-200vw] max-w-screen h-screen bg-white text-black transition-transform duration-700 flex-col flex justify-center items-center gap-5 text-2xl lg:hidden absolute inset-0 z-40"
-      }`}
+        isOpen ? "translate-x-0" : "translate-x-[-200vw]"
+      } max-w-screen h-screen bg-white text-black transition-transform duration-700 flex-col flex justify-center items-center gap-5 text-2xl lg:hidden fixed inset-0 z-40`}
     >
       {" "}
       {navItems.map((navItem) => (
